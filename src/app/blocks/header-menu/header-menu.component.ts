c68361bdb73e01bba4b4f585ee020aa0647ea2be
Jonas Van der Biest <jvanderbiest@gmail.com>
2021-04-30 16:51:31 +0200

    temporary NL-ES-GE-TR

--- src/app/blocks/header-menu/header-menu.component.ts
+++ src/app/blocks/header-menu/header-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
 import { faDoorOpen, faBars } from '@fortawesome/free-solid-svg-icons';
 import { NavigationService } from '../../navigation.service';
 
@@ -11,10 +11,23 @@ export class HeaderMenuComponent implements OnInit {
   faDoorOpen = faDoorOpen;
   faBars = faBars;
   openMobileMenu: boolean = false;
+  currentLanguage : any = undefined;
 
-  constructor(private navigationService: NavigationService) { }
+  languages : any = [
+    { code: 'nl', label: 'Dutch' },
+    { code: 'en', label: 'English' },
+    { code: 'ge', label: 'Georgian' },
+    { code: 'es', label: 'Spanish' },
+    { code: 'tr', label: 'Turkish' },
+  ];
+
+  constructor(private navigationService: NavigationService, @Inject(LOCALE_ID) protected localeId: string) { }
 
   ngOnInit(): void {
+    this.currentLanguage = this.languages.find((x : any) => x.code == this.localeId);
+    if (!this.currentLanguage) {
+      this.currentLanguage = this.languages.find((x : any) => x.code == 'en');
+    }
   }
 
   toggleMobileMenu() {
